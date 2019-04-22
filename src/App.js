c1037422dfce2d4506cd1b6670a48f9dc7a7d930
austin-rey <austinrey95@gmail.com>
2019-04-22 12:51:13 -0500

    Added functional component Header to main app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Contact from "./components/Contact";
-
+import Header from "./components/Header";
 //Main app component
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Container</h1>
+        <Header />
         <Contact />
       </div>
     );
