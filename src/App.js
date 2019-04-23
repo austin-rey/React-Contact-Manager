de1674b5f452c6f902f5305fa3745e19a2fa8f77
austin-rey <austinrey95@gmail.com>
2019-04-23 17:55:01 -0500

    Added component Contacts to better manage each contact component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
 import Header from "./components/Header";
+import Contacts from "./components/Contacts";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-//Main app component
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe@gmail.com"
-            phone="555-555-5555"
-          />
-
-          <Contact
-            name="Austin Reynaud"
-            email="arey@gmail.com"
-            phone="666-666-6666"
-          />
-        </div>
+        <Contacts />
+        <div className="container" />
       </div>
     );
   }
