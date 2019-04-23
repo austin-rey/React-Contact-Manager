b89024d6c071815fbaa359bf22419c4d79b56bb7
austin-rey <austinrey95@gmail.com>
2019-04-23 17:04:04 -0500

    Restructured how props are passed to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
 //Main app component
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+
+        <Contact
+          name="Austin Reynaud"
+          email="arey@gmail.com"
+          phone="666-666-6666"
+        />
       </div>
     );
   }
