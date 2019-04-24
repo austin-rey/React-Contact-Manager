96ce88714aa6de95f3c1ecce20b1e0cd0aee249e
austin-rey <austinrey95@gmail.com>
2019-04-23 19:06:34 -0500

    Added state to show/hide contact entries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,30 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     //Destructuring of props
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+        <h4>
+          {name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
