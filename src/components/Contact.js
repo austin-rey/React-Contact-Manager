2633001c937004e0100b9a48c1d38fc58b711f1b
austin-rey <austinrey95@gmail.com>
2019-04-27 14:16:15 -0500

    Added reducer and dispatch functions to handle contact deletion in contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import { Consumer } from "../context";
 class Contact extends Component {
   state = {
     showContactInfo: false
@@ -10,44 +10,51 @@ class Contact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id, dispatch) => {
+    //Dispatch takes in an action (Type) and a payload
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
 
   render() {
     //Destructuring of props
-    const { name, email, phone } = this.props.contact;
+    const { name, email, phone, id } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}
-          <i
-            style={{ cursor: "pointer" }}
-            onClick={this.onShowClick}
-            className="fas fa-sort-down"
-          />
-          <i
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-            onClick={this.onDeleteClick}
-            className="fas fa-times"
-          />
-        </h4>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">{email}</li>
-            <li className="list-group-item">{phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}
+                <i
+                  style={{ cursor: "pointer" }}
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                />
+                <i
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">{email}</li>
+                  <li className="list-group-item">{phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 //Validate properties
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
--- src/context.js
+++ src/context.js
@@ -2,7 +2,21 @@
 import React, { Component } from "react";
 //Create Context Object
 const Context = React.createContext();
-
+//action will be an object with a type which will be evaluated in the function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        //This is a rest operator, we only want to change contacts in our state
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
 export class Provider extends Component {
   state = {
     contacts: [
@@ -24,7 +38,10 @@ export class Provider extends Component {
         email: "arey@gmail.com",
         phone: "111-111-1111"
       }
-    ]
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
   };
   render() {
     return (
