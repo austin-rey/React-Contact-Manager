8ea7f7334d9fa41c196017667035c1f73f9985aa
austin-rey <austinrey95@gmail.com>
2019-04-27 14:21:04 -0500

    Restructured components sub-directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
+import Header from "./components/layout/Header";
 import Contacts from "./components/Contacts";
 import { Provider } from "./context";
 
