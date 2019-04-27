8386c2a817cf168cdb64e68b528f1cc0e098272e
austin-rey <austinrey95@gmail.com>
2019-04-27 14:50:09 -0500

    Added ContactForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/layout/Header";
-import Contacts from "./components/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,6 +13,7 @@ class App extends Component {
       <Provider>
         <div className="App">
           <Header branding="Contact Manager" />
+          <AddContact />
           <Contacts />
           <div className="container" />
         </div>
