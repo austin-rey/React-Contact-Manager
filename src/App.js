319643a3e8d764e5d22350ef556c77ccdeb1ca7b
austin-rey <austinrey95@gmail.com>
2019-04-30 14:05:06 -0600

    Added react router and routed are pages with correct components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { Component } from "react";
-import Header from "./components/layout/Header";
-import AddContact from "./components/contacts/AddContact";
-import Contacts from "./components/contacts/Contacts";
-import { Provider } from "./context";
+import React, { Component } from 'react';
+//parent component that stores all routes
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
+import Contacts from './components/contacts/Contacts';
+import About from './components/pages/About';
+
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <AddContact />
-          <Contacts />
-          <div className="container" />
-        </div>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container" />
+            {/* All routes go in here */}
+            <Switch>
+              {/* Route Takes path and component */}
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/contact/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
