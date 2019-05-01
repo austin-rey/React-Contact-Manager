13a45ed8dc76beca577b846be46d549d84aba21a
austin-rey <austinrey95@gmail.com>
2019-05-01 14:36:29 -0600

    Added a folder for unnecessary files within our application and listed potential lifecycle methods

---
+++ src/components/test/Test.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  //Lifecyle methods
+  state = {
+    test: 'test'
+  };
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+  //Used with redux
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log('componentWillReceiveProps');
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return null;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
