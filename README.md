419d5cc5c0d6b80e783105bff75b49e943757784
austin-rey <austinrey95@gmail.com>
2019-05-07 13:35:53 -0500

    Added dep for deploying application on github pages

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Check it out here:
+https://austin-rey.github.io/React-Contact-Manager
--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //parent component that stores all routes
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from './components/layout/Header';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
-import Contacts from './components/contacts/Contacts';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
+import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import Contacts from "./components/contacts/Contacts";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import Test from "./components/test/Test";
 
-import { Provider } from './context';
+import { Provider } from "./context";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
