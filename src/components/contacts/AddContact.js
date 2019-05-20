aa96ca5ca7a9c3e2ad696a0efa4288419a19b481
austin-rey <austinrey95@gmail.com>
2019-05-20 14:15:40 -0600

    Updated action creators for deleting and adding contacts

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,6 @@ import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addContact } from '../../actions/contactActions';
-import uuid from "uuid"
 
 class AddContact extends Component {
   state = {
@@ -35,7 +34,6 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
@@ -103,7 +101,7 @@ class AddContact extends Component {
 }
 AddContact.propTypes = {
   addContact: PropTypes.func.isRequired
-}
+};
 //Not bringing any data in
 export default connect(
   null,
